28d8c94841fc6dbd78bbebebfbc470fc68163045
Keith Bannister <keith.bannister@csiro.au>
2022-02-07 15:29:28 +1100

    Tidy up for packaging

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='toohot',
+      version='1.0',
+      description='Script / service to shutdown if intake temperature is too hot',
+      author='Keith Bannister',
+      author_email='keith.bannister@csiro.au',
+      url='',
+      packages=['toohot'],
+      package_data={'toohot':['system_files/*']},
+      entry_points={
+          'console_scripts': [
+              'toohot = toohot:_main',
+              ],
+          },
+     )
